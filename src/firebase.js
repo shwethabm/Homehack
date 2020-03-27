581f58977f3349601b26236e73e7dcdd725331d0
shwethabm <32199303+shwethabm@users.noreply.github.com>
2020-03-27 17:54:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBbJrhBSj3QC924fQkbRY-L2FYuPutsanA',
-  authDomain: 'intcom-a33ee.firebaseapp.com',
-  databaseURL: 'https://intcom-a33ee.firebaseio.com',
-  storageBucket: 'intcom-a33ee.appspot.com'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: ''
 };
 
 firebase.initializeApp(config);
